07e52545456e9d59a067cbc5fdff472ad5e50a1b
Hunter Quant <hunterdquant@gmail.com>
2016-04-15 09:48:19 -0400

    shader optimization

--- js/rotateImage.js
+++ js/rotateImage.js
@@ -16,12 +16,13 @@ var image;
 
 /* Initializes WebGL and globals */
 function init() {
-
   // Init webgl and specify clipspace.
   var canvas = document.getElementById("gl-canvas");
   image = document.getElementById("rotateIt");
   gl = WebGLUtils.setupWebGL(canvas);
-  if (!gl) {alert("Web gl is not available");}
+  if (!gl) {
+    alert("Web gl is not available");
+  }
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(0.0, 0.0, 0.0, 0.0);
   gl.enable(gl.DEPTH_TEST);
@@ -46,7 +47,7 @@ function init() {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-  var indexBuffer  = gl.createBuffer();
+  var indexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indexList), gl.STATIC_DRAW);
 
@@ -65,10 +66,8 @@ function init() {
   var texCoordLocation = gl.getAttribLocation(shaderProgram, "texCoord");
   gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(texCoordLocation);
+  thetaLoc = gl.getUniformLocation(shaderProgram, "theta");
 
-  rotXLoc = gl.getUniformLocation(shaderProgram, "RotX");
-  rotYLoc = gl.getUniformLocation(shaderProgram, "RotY");
-  rotZLoc = gl.getUniformLocation(shaderProgram, "RotZ");
 
   render();
 }
@@ -78,60 +77,41 @@ function render() {
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   var vertexCount = indexList.length;
-
   theta += 0.015;
-
-  var c = Math.cos(theta);
-  var s = Math.sin(theta);
-  var RotZ = [c, -s, 0.0, 0.0, s, c, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0];
-  var RotY = [c, 0.0, -s, 0.0, 0.0, 1.0, 0.0, 0.0, s, 0.0, c, 0.0, 0.0, 0.0, 0.0, 1.0];
-  var RotX = [1.0, 0.0, 0.0, 0.0, 0.0, c, -s, 0.0, 0.0, s, c, 0.0, 0.0, 0.0, 0.0, 1.0];
-
-  gl.uniformMatrix4fv(rotXLoc, false, RotX);
-  gl.uniformMatrix4fv(rotYLoc, false, RotY);
-  gl.uniformMatrix4fv(rotZLoc, false, RotZ);
-
-  gl.drawElements( gl.TRIANGLES, 36, gl.UNSIGNED_BYTE, 0);
+  gl.uniform1f(thetaLoc, theta);
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_BYTE, 0);
   requestAnimFrame(render);
 }
 
 /* Returns an array of vertices */
 function getVertices() {
-  var vertices = [
-    -0.55, -0.55,  0.55,
-     0.55, -0.55,  0.55,
-     0.55,  0.55,  0.55,
-    -0.55,  0.55,  0.55,
+  var vertices = [-0.55, -0.55, 0.55,
+    0.55, -0.55, 0.55,
+    0.55, 0.55, 0.55, -0.55, 0.55, 0.55,
 
     // Back face
     -0.55, 0.55, -0.55,
-     0.55,  0.55, -0.55,
-     0.55,  -0.55, -0.55,
-     -0.55, -0.55, -0.55,
+    0.55, 0.55, -0.55,
+    0.55, -0.55, -0.55, -0.55, -0.55, -0.55,
 
     // Top face
-     -0.55, 0.55, 0.55,
-      0.55, 0.55, 0.55,
-      0.55, 0.55, -0.55,
-     -0.55, 0.55, -0.55,
+    -0.55, 0.55, 0.55,
+    0.55, 0.55, 0.55,
+    0.55, 0.55, -0.55, -0.55, 0.55, -0.55,
 
     // Bottom face
     -0.55, -0.55, -0.55,
-     0.55, -0.55, -0.55,
-     0.55, -0.55,  0.55,
-    -0.55, -0.55,  0.55,
+    0.55, -0.55, -0.55,
+    0.55, -0.55, 0.55, -0.55, -0.55, 0.55,
 
     // Right face
-     0.55, -0.55, -0.55,
-     0.55,  0.55, -0.55,
-     0.55,  0.55,  0.55,
-     0.55, -0.55,  0.55,
+    0.55, -0.55, -0.55,
+    0.55, 0.55, -0.55,
+    0.55, 0.55, 0.55,
+    0.55, -0.55, 0.55,
 
     // left face
-    -0.55, -0.55, -0.55,
-    -0.55, -0.55,  0.55,
-    -0.55,  0.55,  0.55,
-    -0.55,  0.55, -0.55
+    -0.55, -0.55, -0.55, -0.55, -0.55, 0.55, -0.55, 0.55, 0.55, -0.55, 0.55, -0.55
   ];
   return vertices;
 }
@@ -139,47 +119,47 @@ function getVertices() {
 function getTexCoords() {
   var vertices = [
     // Front
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
     // Back
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
     // Top
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
     // Bottom
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
     // Right
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
     // Left
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0
   ];
   return vertices;
 }
 
 function getIndexList() {
   var indices = [
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // right
-    20, 21, 22,     20, 22, 23    // left
+    0, 1, 2, 0, 2, 3, // front
+    4, 5, 6, 4, 6, 7, // back
+    8, 9, 10, 8, 10, 11, // top
+    12, 13, 14, 12, 14, 15, // bottom
+    16, 17, 18, 16, 18, 19, // right
+    20, 21, 22, 20, 22, 23 // left
   ];
   return indices;
 }
